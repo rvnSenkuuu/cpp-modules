cf9c34773dcd985851fd6edfe4a2093ce2ddb5a9
Senku <tolu_48@hotmail.fr>
2024-11-24 21:58:53 +0100

    add: wip c++ boilerplate

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -11,3 +11,32 @@
 /* ************************************************************************** */
 
 #include "Account.hpp"
+
+Account::Account(int initial_deposit)
+{
+	this->_amount = initial_deposit;
+}
+
+Account::~Account(void)
+{
+}
+
+int	Account::getNbAccounts(void)
+{
+	return _nbAccounts;
+}
+
+int	Account::getTotalAmount(void)
+{
+	return _totalAmount;
+}
+
+int	Account::getNbDeposits(void)
+{
+	return _totalNbDeposits;
+}
+
+int	Account::getNbWithdrawals(void)
+{
+	return _totalNbWithdrawals;
+}
