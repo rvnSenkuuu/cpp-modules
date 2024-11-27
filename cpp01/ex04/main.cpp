276b492207a024ab87c56818b588742db3e50c85
rvnSenkuuu <tolu_48@hotmail.fr>
2024-11-27 14:09:39 +0100

    add: ex04

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -11,7 +11,6 @@
 /* ************************************************************************** */
 
 #include <string>
-#include <cstdlib>
 #include <iostream>
 #include <fstream>
 
@@ -21,14 +20,51 @@ static void	print_usage(void)
 				 "Usage: ./sed <filename> <to_find> <to_replace>" << std::endl;
 }
 
+static bool	check_stream(std::ifstream &ifs, std::ofstream &ofs)
+{
+	if (!ifs.is_open())
+	{
+		std::cerr << "Fail open ifstream" << std::endl;
+		return false;
+	}
+	if (!ofs.is_open())
+	{
+		std::cerr << "Fail open ofstream" << std::endl;
+		return false;
+	}
+	return true;
+}
+
 int	main(int argc, char **argv)
 {
-	std::ifstream	ifs;
-	std::ofstream	ofs;
-
 	if (argc != 4)
+	{
 		print_usage();
-	
-	
+		return 1;
+	}
+
+	std::string		line;
+	std::string		output = argv[1];
+	std::string		s1 = argv[2];
+	std::string		s2 = argv[3];
+	output.append(".replace");
+
+	std::ifstream	ifs(argv[1]);
+	std::ofstream	ofs(output.c_str());
+
+	if (!check_stream(ifs, ofs)) return 1;
+	while (getline(ifs, line))
+	{
+		size_t	pos = 0;
+		while ((pos = line.find(s1, pos)) != std::string::npos)
+		{
+			line.erase(pos, s1.length());
+			line.insert(pos, s2);
+			pos += s2.length();
+		}
+		ofs << line << std::endl;
+	}
+	ifs.close();
+	ofs.close();
 	return 0;
 }
