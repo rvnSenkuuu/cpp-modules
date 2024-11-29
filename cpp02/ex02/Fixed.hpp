7759fdefe5cc3406bf26ae21a469dd1f109b3d61
rvnSenkuuu <tolu_48@hotmail.fr>
2024-11-29 18:41:39 +0100

    add: ex02 wip, need to do 4more operator overload

---
+++ cpp02/ex02/Fixed.hpp
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkara2 <tkara2@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/29 10:44:29 by tkara2            #+#    #+#             */
+/*   Updated: 2024/11/29 10:44:29 by tkara2           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+# include <iostream>
+# include <cmath>
+
+class	Fixed
+{
+	public:
+		Fixed();
+		Fixed(int const value);
+		Fixed(float const value);
+		Fixed(Fixed const &other);
+		~Fixed();
+
+		Fixed	&operator=(Fixed const &other);
+		Fixed	operator+(Fixed const &other) const;
+		Fixed	operator-(Fixed const &other) const;
+		Fixed	operator*(Fixed const &other) const;
+		Fixed	operator/(Fixed const &other) const;
+		bool	operator<(Fixed const &other) const;
+		bool	operator<=(Fixed const &other) const;
+		bool	operator>(Fixed const &other) const;
+		bool	operator>=(Fixed const &other) const;
+		bool	operator==(Fixed const &other) const;
+		bool	operator!=(Fixed const &other) const;
+
+		static Fixed			&min(Fixed &lhs, Fixed &rhs);
+		static const Fixed		&min(Fixed const &lhs, Fixed const &rhs);
+		static Fixed			&max(Fixed &lhs, Fixed &rhs);
+		static const Fixed		&max(Fixed const &lhs, Fixed const &rhs);
+
+		void	set_raw_bits(int const raw);
+		int		get_raw_bits(void) const;
+		int		to_int(void) const;
+		float	to_float(void) const;
+
+	private:
+		int					_value;
+		static const int	_rawbits = 8;
+};
+
+std::ostream	&operator<<(std::ostream &os, Fixed const &src);
+
+#endif
---
+++ cpp02/ex02/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkara2 <tkara2@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/29 10:44:29 by tkara2            #+#    #+#             */
+/*   Updated: 2024/11/29 10:44:29 by tkara2           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int	main(void) 
+{
+	Fixed		a;
+	Fixed const	b(Fixed(5.05f) * Fixed(2));
+
+	std::cout << a << std::endl;
+	// std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	// std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << Fixed::max(a, b) << std::endl;
+	return 0;
+}
