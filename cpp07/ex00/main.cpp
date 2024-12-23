1d49ddf89e2b72607084afd7daab7eca1bec24d4
rvnSenkuuu <tolu_48@hotmail.fr>
2024-12-23 15:44:25 +0100

    add: str test case

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -18,15 +18,32 @@ int	main(void)
 	int	lhs = 1;
 	int	rhs = 9;
 
-	std::cout << "-----Before swap-----" << std::endl;
+	std::cout << "-----Before swap (int)-----" << std::endl;
 	std::cout << "LHS: " << lhs << " and RHS: " << rhs << std::endl;
 	std::cout << "Min(lhs, rhs) = " << ::min(lhs, rhs) << std::endl;
 	std::cout << "Max(lhs, rhs) = " << ::max(lhs, rhs) << std::endl;
 	::swap(lhs, rhs);
 	std::cout << std::endl;
-	std::cout << "-----After swap-----" << std::endl;
+	std::cout << "-----After swap (int)-----" << std::endl;
 	std::cout << "LHS: " << lhs << " and RHS: " << rhs << std::endl;
 	std::cout << "Min(lhs, rhs) = " << ::min(lhs, rhs) << std::endl;
 	std::cout << "Max(lhs, rhs) = " << ::max(lhs, rhs) << std::endl;
+
+	std::cout << std::endl;
+
+	std::string	str_lhs = "chaine2";
+	std::string	str_rhs = "chaine1";
+
+	std::cout << "-----Before swap (str)-----" << std::endl;
+	std::cout << "Str_lhs: " << str_lhs << " and Str_rhs: " << str_rhs << std::endl;
+	std::cout << "Min(Str_lhs, Str_rhs) = " << ::min(str_lhs, str_rhs) << std::endl;
+	std::cout << "Max(Str_lhs, Str_rhs) = " << ::max(str_lhs, str_rhs) << std::endl;
+	::swap(str_lhs, str_rhs);
+	std::cout << std::endl;
+	std::cout << "-----After swap (str)-----" << std::endl;
+	std::cout << "Str_lhs: " << str_lhs << " and Str_rhs: " << str_rhs << std::endl;
+	std::cout << "Min(Str_lhs, Str_rhs) = " << ::min(str_lhs, str_rhs) << std::endl;
+	std::cout << "Max(Str_lhs, Str_rhs) = " << ::max(str_lhs, str_rhs) << std::endl;
+
 	return 0;
 }
