78289170c7bf07d172c436776d1d2decdabce155
rvnSenkuuu <tolu_48@hotmail.fr>
2025-01-06 10:50:05 +0100

    add: PmergeMe class, displayData template, loading the value into the containers

---
+++ cpp09/ex02/PmergeMe.cpp
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkara2 <tkara2@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/06 10:15:55 by tkara2            #+#    #+#             */
+/*   Updated: 2025/01/06 10:48:40 by tkara2           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "PmergeMe.hpp"
+
+PmergeMe::PmergeMe(void)
+{
+	std::cout << "PmergeMe default constructor called" << std::endl; 
+}
+
+PmergeMe::PmergeMe(int argc, char **argv)
+{
+	std::cout << "PmergeMe assigmement constructor called" << std::endl;
+	checkArg(argc, argv);
+	loadArg(argc, argv);
+	std::cout << "Deque container value: " << std::endl;
+	displayData(this->_deqData, false);
+	std::cout << "Vector container value: " << std::endl;
+	displayData(this->_vecData, false);
+}
+
+PmergeMe::PmergeMe(PmergeMe const &other)
+{
+	std::cout << "PmergeMe copy assigment constructor called" << std::endl;
+	*this = other;
+}
+
+PmergeMe::~PmergeMe(void)
+{
+	std::cout << "PmergeMe destructor called" << std::endl;
+}
+
+PmergeMe	&PmergeMe::operator=(PmergeMe const &other)
+{
+	std::cout << "PmergeMe copy assignement opearator called" << std::endl;
+	if (this != &other)
+	{
+		this->_deqData = other._deqData;
+		this->_vecData = other._vecData;
+	}
+	return *this;
+}
+
+void	PmergeMe::checkArg(int argc, char **argv)
+{
+	if (argc <= 2)
+		throw std::invalid_argument("Wrong argument number\n"
+									"Usage: ./PmergeMe <args1>...<argsN>");
+
+	for (int i = 1; i < argc; i++)
+	{
+		for (size_t j = 0; j < std::strlen(argv[i]); j++)
+			if (!std::isdigit(argv[i][j]))
+				throw std::invalid_argument("Non digit argument detected");
+	}
+
+	for (int i = 1; i < argc; i++)
+	{
+		long number = std::atol(argv[i]);
+		if (number < 0 || number > std::numeric_limits<int>::max())
+			throw std::invalid_argument("Negatif numbers or more than INT_MAX are not handled");
+	}
+
+	std::set<std::string>	args;
+	for (int i = 1; i < argc; i++)
+	{
+		if (!args.insert(argv[i]).second)
+				throw std::invalid_argument("Same arguments are not handled");
+	}
+}
+
+void	PmergeMe::loadArg(int argc, char **argv)
+{
+	for (int i = 1; i < argc; i++)
+	{
+		long	value = std::atol(argv[i]);
+		this->_deqData.push_back(value);
+		this->_vecData.push_back(value);
+	}
+}
---
+++ cpp09/ex02/PmergeMe.hpp
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkara2 <tkara2@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/06 10:15:14 by tkara2            #+#    #+#             */
+/*   Updated: 2025/01/06 10:48:31 by tkara2           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PMERGEME_HPP
+#define PMERGEME_HPP
+
+# include <iostream>
+# include <cstdlib>
+# include <cstring>
+# include <limits>
+# include <algorithm>
+# include <vector>
+# include <deque>
+# include <set>
+
+template<typename T>
+void	displayData(T data, bool verbose)
+{
+	typename T::iterator	it = data.begin();
+	typename T::iterator	ite = data.end();
+
+	if (verbose == false)
+		std::cout << "Before: ";
+	else
+		std::cout << "After: ";
+	for (; it != ite; it++)
+		std::cout << *it << " ";
+	std::cout << std::endl;
+}
+
+class	PmergeMe
+{
+	public:
+		PmergeMe(int argc, char **argv);
+		~PmergeMe(void);
+
+		void	checkArg(int argc, char **argv);
+		void	loadArg(int argc, char **argv);
+
+	private:
+		PmergeMe(void);
+		PmergeMe	&operator=(PmergeMe const &other);
+		PmergeMe(PmergeMe const &other);
+
+	private:
+		std::deque<int>	_deqData;
+		std::vector<int>	_vecData;
+};
+
+#endif
